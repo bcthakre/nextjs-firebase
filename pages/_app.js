3690c463c4e4070c0935983c52ead334487848a7
bcthakre <bcthakregcp@gmail.com>
2022-09-30 18:15:48 -0500

    Updated env variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from '../components/Navbar';
+import { AuthContextProvider } from '../context/AuthContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AuthContextProvider>
+        <NavbarComp />
+        <Component {...pageProps} />
+      </AuthContextProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/signup.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useAuth } from '../context/AuthContext';
+
+const Signup = () => {
+  const { user, signup } = useAuth();
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signup(data.email, data.password);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div
+      style={{
+        width: '40%',
+        margin: 'auto',
+      }}
+    >
+      <h1 className='text-center my-3'>Signup</h1>
+      <Form onSubmit={handleSignup}>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter Email'
+            required
+            onChange={(e) =>
+              setData({
+                ...data,
+                email: e.target.value,
+              })
+            }
+            value={data.email}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            required
+            onChange={(e) =>
+              setData({
+                ...data,
+                password: e.target.value,
+              })
+            }
+            value={data.password}
+          />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Signup
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Signup;
