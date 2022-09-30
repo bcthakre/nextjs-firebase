6b8baaf42bafcf15ec87d8c30f2720cc326bb005
bcthakre <bcthakregcp@gmail.com>
2022-09-30 18:25:39 -0500

    completed the project

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,24 @@ import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from '../components/Navbar';
 import { AuthContextProvider } from '../context/AuthContext';
+import ProtectedRoute from '../components/ProtectedRoute';
+import { useRouter } from 'next/router';
+
+const noAuthRequired = ['/login', '/signup'];
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <AuthContextProvider>
         <NavbarComp />
-        <Component {...pageProps} />
+        {noAuthRequired.includes(router.pathname) ? (
+          <Component {...pageProps} />
+        ) : (
+          <ProtectedRoute>
+            <Component {...pageProps} />
+          </ProtectedRoute>
+        )}
       </AuthContextProvider>
     </>
   );
